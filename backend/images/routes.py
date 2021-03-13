dafd001734179214ae7bd359b799757f02a65bc7
Joseph Vitko <josephvitko21@gmail.com>
2021-03-13 15:09:09 -0500

    added requirements.txt, basic image saving

--- backend/images/routes.py
+++ backend/images/routes.py
@@ -1,4 +1,20 @@
-from flask import Blueprint
+from flask_praetorian import auth_required, current_user
+from flask import Blueprint, request
+from backend import guard, db, bcrypt
+from backend.models import User, create_all_model_db
+from backend.images.utils import save_picture
 
 
 images = Blueprint('images', __name__)
+
+
+@images.route('/api/upload_image', methods=['POST'])
+@auth_required
+def upload_image():
+    if not request.files:
+        return 'Error: No image was included with upload'
+    else:
+        file = request.files['file']
+        save_picture(file)
+        return 'image saved', 200
+
