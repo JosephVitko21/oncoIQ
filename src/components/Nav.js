b32013ef3b01b934bd2b809f589ecbba46633080
Gary Peng <gary-peng@users.noreply.github.com>
2021-03-13 22:56:07 -0500

    Style logout

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
+import Button from 'react-bootstrap/Button';
 import {login, authFetch, useAuth, logout} from "../auth";
 
 export default function Nav() {
+    const [logged] = useAuth();
+
     return (
         <div>
             <Navbar bg="primary" variant="dark">
                 <Navbar.Brand href="#home">oncoIQ</Navbar.Brand>
-                <button onClick={() => logout()}>Logout</button>
+                <Navbar.Collapse className="justify-content-end">
+                    {
+                        logged? <Button variant="outline-light" onClick={() => logout()}>Logout</Button> : null
+                    }
+                </Navbar.Collapse>
             </Navbar>
         </div>
     );
