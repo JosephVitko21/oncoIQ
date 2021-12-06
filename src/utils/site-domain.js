f2dcd7bf19d63d21b1f489c5163396943801933c
Joseph Vitko <josephvitko21@gmail.com>
2021-12-06 14:31:32 -0500

    change back to production domain

--- src/utils/site-domain.js
+++ src/utils/site-domain.js
@@ -1,4 +1,4 @@
 //const domain = "http://localhost:5000"
-//const domain = "https://api.oncoiq.io";
-const domain = "http://10.3.10.75:5000"
+const domain = "https://api.oncoiq.io";
+//const domain = "http://10.3.10.75:5000"
 export default domain;
