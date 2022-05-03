5802017b0b9074e2bff5870a457ba99592b7af49
Gary Peng <49541146+gary-peng@users.noreply.github.com>
2022-05-03 03:25:27 -0400

    Community and case card rebuilt

--- src/components/landing/HowCards.js
+++ src/components/landing/HowCards.js
@@ -4,7 +4,11 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function HowCards(props) {
     return (
-        <Flex flexDirection="column" justifyContent="space-around" alignItems="center" textAlign="center" w="20vw" h="15vw" borderRadius="7px" p="15px" boxShadow="rgb(186 186 186 / 40%) 0px 4px 16px" bg="white" mr="30px">
+        <Flex 
+            flexDirection="column" justifyContent="space-around" alignItems="center" textAlign="center" w="20vw" h="15vw" p="15px" mr="30px"
+            borderRadius="7px" boxShadow="rgb(186 186 186 / 40%) 0px 4px 16px" bg="white"
+            transition="transform .2s" _hover={{ transform: "scale(1.05)" }}
+        >
             <Heading fontSize="lg">{props.title}</Heading>
             <Text fontSize="50px" color="primary"><FontAwesomeIcon icon={props.icon} /></Text>
             <Text fontSize="md">{props.children}</Text>
--- src/pages/Community.js
+++ src/pages/Community.js
@@ -1,17 +1,18 @@
 import React from "react";
 import CaseList from "../components/forum/list/CaseList";
 
+import { Box, Flex, Heading, Text, Image, Icon } from '@chakra-ui/react'
+
 export default function Community() {
     return (
-        <div className="bg-navy min-vh-100">
-            <div className="container mt-5">
-                <h1 className="text-white">Help a Pathologist</h1>
-                <p className="text-white mb-4">
-                    Explore cases the pathology community needs help diagnosing
-                </p>
-                <br/>
+        <Box minH="100vh" bg="linear-gradient(0deg, rgba(246,236,250,1) 0%, rgba(255,255,255,1) 100%)">
+            <Flex flexDirection="column" alignItems="center" mt="10vh">
+                <Box w="60vw" mb="30px">
+                    <Heading mb="10px">Help a Pathologist</Heading>
+                    <Text fontSize="md" color="primary">Explore cases the pathology community needs help diagnosing</Text>
+                </Box>
                 <CaseList/>
-            </div> 
-        </div>
+            </Flex>
+        </Box>
     )
 }
